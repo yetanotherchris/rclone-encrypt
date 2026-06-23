52a55ffeec83b3bc97099f8d5bd4131793a95341
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-06-23 17:24:07 +0000

    chore: update package manifests to v1.0.3

--- Formula/rclone-encrypt.rb
+++ Formula/rclone-encrypt.rb
@@ -1,25 +1,25 @@
 class RcloneEncrypt < Formula
   desc "Encrypt and decrypt files using rclone-compatible encryption (XSalsa20-Poly1305 + scrypt)"
   homepage "https://github.com/yetanotherchris/rclone-encrypt"
-  version "1.0.2"
+  version "1.0.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.2/rclone-encrypt-darwin-arm64.tar.gz"
-      sha256 "d4795d2a621032dc9f1a4b1e7eb75fe753ee202477f136a7f6f26d8f2c93386d"
+      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.3/rclone-encrypt-darwin-arm64.tar.gz"
+      sha256 "24e284301903e1f00698ddf8406f33f8e1e8087cfc51ed956689dea29e0f28a9"
     else
-      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.2/rclone-encrypt-darwin-amd64.tar.gz"
-      sha256 "153ca4dda6803e48f7269fcb2fd6bc9ca2eb8c62c87aae599ce09d545ecbd8b3"
+      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.3/rclone-encrypt-darwin-amd64.tar.gz"
+      sha256 "d2996607f5f5c2bd2c44c2d93fb6c785cfd3b9ec292f55bce0b26b683ec8a2dc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.2/rclone-encrypt-linux-arm64.tar.gz"
-      sha256 "44d6fcf1961a8715f5f3d4bf84f701aeffe3c981780f0ff88091e34a6b4d2d89"
+      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.3/rclone-encrypt-linux-arm64.tar.gz"
+      sha256 "01134ff6ef9d8be6593d3047affcd313eb39030d78d5c7bb3aea50d99e840616"
     else
-      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.2/rclone-encrypt-linux-amd64.tar.gz"
-      sha256 "83bd8e018579126bf660adf19bf9d96437bb39e010ca6d34d87a8fad17f2a682"
+      url "https://github.com/yetanotherchris/rclone-encrypt/releases/download/v1.0.3/rclone-encrypt-linux-amd64.tar.gz"
+      sha256 "8e5b29393fa3a3e89a4e4576dc0d72e462cc9cb852de48eec03415f4e9e5a9e6"
     end
   end
 
